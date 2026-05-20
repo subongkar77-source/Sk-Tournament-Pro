7e334be3de51bdd1f6b463d0e525f99c5bb6d9e8
subongkar77-source <subongkar77@gmail.com>
2026-05-20 16:12:54 +0530

    Specify template folder for Flask app

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 import os
-app = Flask(__name__)
+
+# Yahan humne explicit tarike se bataya hai ki templates kahan hain
+app = Flask(__name__, template_folder='templates')
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+    
