a992ab7c2e55eebbe3f055d4e8e766b90318c635
subongkar77-source <subongkar77@gmail.com>
2026-05-20 15:24:41 +0530

    Refactor template directory path construction
    Updated template directory path handling in Flask app.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 from flask import Flask, render_template
 
-# Path ko force kar rahe hain taaki templates folder mil jaye
-template_dir = os.path.abspath('templates')
+# 'os.path.dirname(__file__)' ye ensure karta hai ki path hamesha app.py ke folder se hi start ho
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
@@ -10,6 +10,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Render PORT variable deta hai, agar na mile toh 5000 use karein
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
