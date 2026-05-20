d9af960502f461116aeaeffe2df8c969551b427d
subongkar77-source <subongkar77@gmail.com>
2026-05-20 17:11:18 +0530

    Refactor Flask app initialization and clean up code
    Updated Flask app initialization to specify template directory and removed debugging print statements.

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+# Ye lines Flask ko batayengi ki templates kahan hain, chahe server kahin bhi ho
+template_dir = os.path.abspath('templates')
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def home():
-    # Debugging print
-    print(f"Current Working Directory: {os.getcwd()}")
-    print(f"Files in folder: {os.listdir('.')}")
-    if os.path.exists('templates'):
-        print(f"Files in templates: {os.listdir('templates')}")
-    else:
-        print("Templates folder not found!")
-    
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
