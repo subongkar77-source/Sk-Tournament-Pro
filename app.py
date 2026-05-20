c4b3a955861e6bfdb8765982eda2c742fa11ba36
subongkar77-source <subongkar77@gmail.com>
2026-05-20 21:27:56 +0530

    Set custom template folder for Flask app
    Updated Flask app to set custom template folder path.

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
 import os
+from flask import Flask, render_template
 
-app = Flask(__name__)
+# यह कोड आपकी वर्तमान डायरेक्टरी के आधार पर 'templates' फोल्डर ढूंढ लेगा
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # रेंडर के लिए पोर्ट बाइंडिंग
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
